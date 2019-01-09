04680e42eb0669e71c4f24abd6f2161a63ce98bf
eggp <eggprojec@gmail.com>
2019-01-10 00:44:30 +0100

    add test code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',pathMatch:'full',redirectTo:'test1/details'},
+  {path:'test1',loadChildren:'./test1/test1.module#Test1Module'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/test1/test1.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Test1RoutingModule } from './test1-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    Test1RoutingModule
+  ]
+})
+export class Test1Module { }
